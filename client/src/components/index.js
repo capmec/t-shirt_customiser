88c60defda12d76a7c872b1bd8b158eccc471e7d
capmec <capmec@gmail.com>
2023-07-08 17:59:39 +0200

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import AIPicker from './AIPicker'
+import CustomButton from './CustomButton'
+import ColorPicker from './ColorPicker'
+import FilePicker from './FilePicker'
+import Tab from './Tab'
+
+export { AIPicker, CustomButton, ColorPicker, FilePicker, Tab }
