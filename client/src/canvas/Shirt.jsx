687248d26ded35ef665c75b609a4d79a37bb016f
capmec <capmec@gmail.com>
2023-10-04 19:03:51 +0200

    exceeded open ai daily quota

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-import { easing } from 'maath';
-import { useSnapshot } from 'valtio';
-import { useFrame } from '@react-three/fiber';
-import { Decal, useGLTF, useTexture } from '@react-three/drei';
+import { easing } from 'maath'
+import { useSnapshot } from 'valtio'
+import { useFrame } from '@react-three/fiber'
+import { Decal, useGLTF, useTexture } from '@react-three/drei'
 
-import state from '../store';
+import state from '../store'
 
 const Shirt = () => {
-  const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+  const snap = useSnapshot(state)
+  const { nodes, materials } = useGLTF('/shirt_baked.glb')
 
-  const logoTexture = useTexture(snap.logoDecal);
-  const fullTexture = useTexture(snap.fullDecal);
+  const logoTexture = useTexture(snap.logoDecal)
+  const fullTexture = useTexture(snap.fullDecal)
 
-  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+  useFrame((state, delta) =>
+    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
+  )
 
-  const stateString = JSON.stringify(snap);
+  const stateString = JSON.stringify(snap)
 
   return (
     <group key={stateString}>
--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,16 +1,14 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
-import { Configuration, OpenAIApi } from 'openai'
+import OpenAIApi, { OpenAI } from 'openai'
 
 dotenv.config()
 
 const router = express.Router()
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 })
-
-const openai = new OpenAIApi(config)
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: 'Hello from DALL.E ROUTES' })
@@ -20,14 +18,13 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body
 
-    const response = await openai.createImage({
+    const response = await openai.chat.completions.create({
       prompt,
       n: 1,
       size: '1024x1024',
-      response_format: 'b64_json',
+      model: 'gpt-3.5-turbo',
     })
-
-    const image = response.data.data[0].b64_json
+    image_url = response.data.data[0].url
 
     res.status(200).json({ photo: image })
   } catch (error) {
