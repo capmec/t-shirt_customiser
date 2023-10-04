c065fdf60f30d57eef7a7c547cf3deaf667bb950
capmec <capmec@gmail.com>
2023-10-04 16:25:55 +0200

    file upload done

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -30,7 +30,7 @@ const Customizer = () => {
     stylishShirt: false,
   })
 
-  //show tab content depending on the activeTab
+  // show tab content depending on the activeTab
   const generateTabContent = () => {
     switch (activeEditorTab) {
       case 'colorpicker':
@@ -38,11 +38,46 @@ const Customizer = () => {
       case 'filepicker':
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />
       case 'aipicker':
-        return <AIPicker />
+        return (
+          <AIPicker
+            prompt={prompt}
+            setPrompt={setPrompt}
+            generatingImg={generatingImg}
+            handleSubmit={handleSubmit}
+          />
+        )
       default:
         return null
     }
   }
+
+  const handleSubmit = async (type) => {
+    if (!prompt) return alert('Please enter a prompt')
+
+    try {
+      setGeneratingImg(true)
+
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          prompt,
+        }),
+      })
+
+      const data = await response.json()
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`)
+    } catch (error) {
+      alert(error)
+    } finally {
+      setGeneratingImg(false)
+      setActiveEditorTab('')
+    }
+  }
+
   const handleDecals = (type, result) => {
     const decalType = DecalTypes[type]
 
@@ -60,12 +95,15 @@ const Customizer = () => {
         break
       case 'stylishShirt':
         state.isFullTexture = !activeFilterTab[tabName]
-
+        break
       default:
-        state.isFullTexture = true
-        state.isLogoTexture = false
+        state.isLogoTexture = true
+        state.isFullTexture = false
+        break
     }
-    //after setting the state, update the activeFilterTab
+
+    // after setting the state, activeFilterTab is updated
+
     setActiveFilterTab((prevState) => {
       return {
         ...prevState,
@@ -86,12 +124,12 @@ const Customizer = () => {
       {!snap.intro && (
         <>
           <motion.div
-            key="custom"
-            className="absolute top-0 left-0 z-10"
+            key='custom'
+            className='absolute top-0 left-0 z-10'
             {...slideAnimation('left')}
           >
-            <div className="flex flex-col items-center justify-center min-h-screen">
-              <div className="editortabs-container tabs">
+            <div className='flex items-center min-h-screen'>
+              <div className='editortabs-container tabs'>
                 {EditorTabs.map((tab) => (
                   <Tab
                     key={tab.name}
@@ -99,22 +137,26 @@ const Customizer = () => {
                     handleClick={() => setActiveEditorTab(tab.name)}
                   />
                 ))}
+
                 {generateTabContent()}
               </div>
             </div>
           </motion.div>
 
-          <motion.div className="absolute z-0 top-5 right-5" {...fadeAnimation}>
+          <motion.div
+            className='absolute z-10 top-5 right-5'
+            {...fadeAnimation}
+          >
             <CustomButton
-              type="filled"
-              title="Go Back"
+              type='filled'
+              title='Go Back'
               handleClick={() => (state.intro = true)}
-              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              customStyles='w-fit px-4 py-2.5 font-bold text-sm'
             />
           </motion.div>
 
           <motion.div
-            className="filtertabs-container"
+            className='filtertabs-container'
             {...slideAnimation('up')}
           >
             {FilterTabs.map((tab) => (
