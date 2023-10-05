902a2703b96ffbd34394396a3d31d8ff9e7100d6
capmec <capmec@gmail.com>
2023-10-05 11:17:24 +0200

    ai not working because it is paid

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -6,7 +6,7 @@ const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
   return (
     <div className='aipicker-container'>
       <textarea
-        placeholder='Ask AI...'
+        placeholder='Openai is a paid service, and is not available at the moment.'
         rows={5}
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
